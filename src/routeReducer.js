95ad04f64081147af5a6c606fbd1e3f534a241d5
CaffeineFusion <owenclsmith@gmail.com>
2021-09-01 02:05:56 +1000

    extract string value and remove comments

--- src/routeReducer.js
+++ src/routeReducer.js
@@ -1,10 +1,11 @@
 const { ON_MESSAGE } = require('./constants');
 
 const createRouteReducer = (initialState) => { 
-    return (state=initialState, action) => 
-    {
+    return (state=initialState, action) => {
+        console.log('Route reducer', state, action);
         switch (action.type) {
             case ON_MESSAGE:
+                console.log(`Route reducer - ON_MESSAGE, ${action.payload.text === undefined}, ${action.payload.text === undefined || action.payload.text.route === undefined}`);
                 return action.payload.text === undefined || action.payload.text.route === undefined ? state : action.payload.text.route;
             default:
                 return state;
