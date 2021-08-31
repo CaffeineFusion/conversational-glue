0bab8ade0528988f29d9a11c126d53b629849c38
CaffeineFusion <owenclsmith@gmail.com>
2021-09-01 02:29:12 +1000

    debugging Controller

--- src/RouteController.jsx
+++ src/RouteController.jsx
@@ -5,7 +5,9 @@ const { React, useEffect } = require('react');
 //<Redirect to={route} /> 
 
 const RouteController = (props) => {
+    console.log('Rendering RouteController', props);
     useEffect(() => {
+        console.log('Route Controller Effect activated', props.history, props.activeRoute);
         props.history.push(props.activeRoute)
     }, [props.activeRoute]);
 };
